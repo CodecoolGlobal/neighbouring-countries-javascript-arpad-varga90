0f6be33e27f9f0fc192577b153d846d919b792fd
arpad.varga90 <arpad.varga90@gmail.com>
2024-05-06 14:45:33 +0200

    implement selectHandler

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,15 @@
 import countries from './data.js';
 
+const all = document.querySelector('#all');
+const selectedCountry = [];
+
 function main() {
   addOptions();
+  all.addEventListener('change', handleSelect);
 }
 
 function addOptions() {
-  countries.sort((a, b) =>
-    a.name.common.localeCompare(b.name.common)
-  );
-  const all = document.querySelector('#all');
+  countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
   const optionDefault = document.createElement('option');
   optionDefault.innerText = 'Please choose a country';
   all.append(optionDefault);
@@ -17,6 +18,24 @@ function addOptions() {
     option.innerText = country.name.common;
     all.append(option);
   }
+}
+
+function createNode(tagName, attributes = {}, parentNode = '') {
+  const el = document.createElement(tagName);
+
+  for (const key of Object.keys(attributes)) {
+    el[key] = attributes[key];
+  }
+  if (parentNode === '') {
+    return el;
+  }
+  parentNode.append(el);
+  return null;
+}
+
+function handleSelect(event) {
+  console.log(event.target.value);
+  selectedCountry.push(event.target.value);
 }
 
 main();
