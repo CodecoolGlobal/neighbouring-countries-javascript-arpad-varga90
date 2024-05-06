2e2058f0f1d01ad09ea9656a4c98ac95194bf0ee
spcsabi <csabaspengler@gmail.com>
2024-05-06 11:36:49 +0200

    add countries list

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,11 @@
 import countries from './data.js';
 
+const all = document.querySelector('#all');
+
+for (const countrie of countries) {
+  const option = document.createElement('option');
+  option.innerText = countrie.name.common;
+  all.append(option);
+}
+
 console.log(countries);
