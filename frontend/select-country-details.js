78d5599be7eb777fbce41a4a3ff22c568fd55a19
arpad.varga90 <arpad.varga90@gmail.com>
2024-05-08 22:36:33 +0200

    implement OPTIONAL TASK: Translations solution

--- frontend/select-country-details.js
+++ frontend/select-country-details.js
@@ -4,10 +4,16 @@ import { selectedCountry } from './script.js';
 let arrPointer;
 const getCurrentIndex = () => arrPointer;
 const setCurrentIndex = (value) => (arrPointer = value);
+
+let actualTranslation;
+const getActualTranslation = () => actualTranslation;
+const setActualTranslation = (value) => (actualTranslation = value);
+
 const countryEl = document.getElementById('country');
 
 export function runOnce() {
   navButtons();
+  addTranslations();
   revealButtons();
 }
 
@@ -68,13 +74,22 @@ function getDetailsFragment(lastSelected) {
     region,
     subregion,
     capital,
+    translations,
   } = lastSelected;
+  const lngShort = getActualTranslation();
+  let isTrAvlailable = false;
+  let trName = '';
 
-  const fragment = document.createDocumentFragment();
+  if (Object.keys(translations).includes(lngShort)) {
+    trName = translations[lngShort].common;
+    isTrAvlailable = true;
+  }
 
   //prettier-ignore
   fragment.appendChild(createNode('img', { src: png, alt: `flag-of-${common.toLowerCase()}`}));
-  fragment.appendChild(createNode('h1', { innerText: common }));
+  fragment.appendChild(
+    createNode('h1', { innerText: isTrAvlailable ? trName : common }) //User is not informed if tr is nat available
+  );
   fragment.appendChild(createNode('h2', { innerText: region }));
   fragment.appendChild(createNode('h3', { innerText: subregion }));
   fragment.appendChild(createNode('h4', { innerText: capital }));
@@ -146,4 +161,56 @@ function handlePrevClick() {
 
   countryEl.innerHTML = '';
   countryEl.appendChild(getDetailsFragment(selectedCountry[currentIndex]));
+}
+
+function addTranslations() {
+  const select = document.querySelector('#all');
+  const trList = Object.keys(countries[0].translations);
+  const languages = {
+    ara: 'Arabic',
+    ces: 'Czech',
+    cym: 'Welsh',
+    deu: 'German',
+    est: 'Estonian',
+    fin: 'Finnish',
+    fra: 'French',
+    hrv: 'Croatian',
+    hun: 'Hungarian',
+    ita: 'Italian',
+    jpn: 'Japanese',
+    kor: 'Korean',
+    nld: 'Dutch',
+    per: 'Persian',
+    pol: 'Polish',
+    por: 'Portuguese',
+    rus: 'Russian',
+    slk: 'Slovak',
+    spa: 'Spanish',
+    swe: 'Swedish',
+    urd: 'Urdu',
+    zho: 'Chinese',
+  };
+
+  const newSelect = createNode('select', { id: 'translations' });
+  newSelect.appendChild(createNode('option', { selected: null }));
+  for (const translation of trList) {
+    newSelect.appendChild(
+      createNode('option', {
+        value: `${translation}`,
+        innerText: `${languages[translation]}`,
+      })
+    );
+  }
+
+  newSelect.addEventListener('change', handleTrChange);
+
+  select.after(newSelect);
+}
+
+function handleTrChange(event) {
+  const selectedTranslation = event.target.value;
+  setActualTranslation(selectedTranslation);
+
+  countryEl.innerHTML = '';
+  countryEl.appendChild(getDetailsFragment(selectedCountry.at(-1)));
 }
